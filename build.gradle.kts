8954e50b45f1adf161fe8055f7fa5d450b36b8c8
Redtronics <nils.jaekel@proton.me>
2024-02-26 22:15:07 +0100

    chore: removed redundant stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.serialization.json) apply false
 }
 
 group = "dev.redtronics"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
-}
