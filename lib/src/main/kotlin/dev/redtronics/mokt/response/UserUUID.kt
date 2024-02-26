af715714a57285228e652465f0aa5b726e34749f
Redtronics <nils.jaekel@proton.me>
2024-02-26 23:09:10 +0100

    feat: added Mokt class with get user and users

---
+++ lib/src/main/kotlin/dev/redtronics/mokt/response/UserUUID.kt
@@ -0,0 +1,36 @@
+/*
+ * MIT License
+ * Copyright 2024 Redtronics
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+package dev.redtronics.mokt.response
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserUUID(
+    val name: String,
+    val id: String
+)
+
+@Serializable
+data class UserUUIDNoContent(
+    val path: String,
+    val errorMessage: String
+)
+
+@Serializable
+data class UserUUIDNotFound(
+    val error: String,
+    val errorMessage: String
+)
