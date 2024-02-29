4ecd222cdac8da8f40413fd868f830160cf75ce6
Redtronics <nils.jaekel@proton.me>
2024-02-29 23:02:24 +0100

    feat: added test for getting blocked server

---
+++ lib/src/test/kotlin/dev/redtronics/mokt/TestBlockedServer.kt
@@ -0,0 +1,36 @@
+/*
+ * MIT License
+ * Copyright 2024 Redtronics
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+package dev.redtronics.mokt
+
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class TestBlockedServer {
+    private val mokt = Mokt()
+
+    @Test
+    fun `test to get blocked server without limit`() = runBlocking {
+        val blockedServer = mokt.getBlockedServer(limit = 0)
+        assertTrue(actual = blockedServer.hashedAddresses.isNotEmpty())
+    }
+
+    @Test
+    fun `test to get blocked server with limit`() = runBlocking {
+        val blockedServer = mokt.getBlockedServer(limit = 20)
+        assertTrue(actual = blockedServer.hashedAddresses.size == 20)
+    }
+}
