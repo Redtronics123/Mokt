ad083314aff7826628c199966a2cc0f58939a333
Redtronics <nils.jaekel@proton.me>
2024-02-29 13:58:40 +0100

    fix: added serial name to parse right argument

--- lib/src/main/kotlin/dev/redtronics/mokt/entity/PlayerUUID.kt
+++ lib/src/main/kotlin/dev/redtronics/mokt/entity/PlayerUUID.kt
@@ -15,11 +15,13 @@
 
 package dev.redtronics.mokt.entity
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class PlayerUUID(
     val name: String,
+    @SerialName("id")
     val uuid: String,
 )
 
