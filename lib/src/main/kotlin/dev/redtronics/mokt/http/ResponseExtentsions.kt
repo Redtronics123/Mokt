0c7c4eaa788ea2db253133e90cb1710f5a85f37f
Redtronics <nils.jaekel@proton.me>
2024-03-01 20:05:39 +0100

    refactor: replaced Response handler with response extention func

---
+++ lib/src/main/kotlin/dev/redtronics/mokt/http/ResponseExtentsions.kt
@@ -0,0 +1,28 @@
+/*
+ * MIT License
+ * Copyright 2024 Redtronics
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+package dev.redtronics.mokt.http
+
+import io.ktor.client.statement.*
+import io.ktor.http.*
+
+@Throws(Exception::class)
+suspend fun HttpResponse.requireSuccessful() {
+    if (status.isSuccess()) return
+    when(status) {
+        HttpStatusCode.TooManyRequests -> throw TooManyRequestsException()
+        else -> throw Exception("Request failed ($status): ${bodyAsText()}")
+    }
+}
