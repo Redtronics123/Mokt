aabc7dac725fa55a4be865dfb311472ecf9accdd
Redtronics <nils.jaekel@proton.me>
2024-03-03 00:19:10 +0100

    feat: added sealed class to differentiate auth

--- example/src/main/kotlin/Main.kt
+++ example/src/main/kotlin/Main.kt
@@ -1,7 +1,35 @@
 package dev.redtronics
 
+import dev.redtronics.mokt.auth.MSAuth
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.serialization.kotlinx.json.*
 import kotlinx.coroutines.supervisorScope
+import java.awt.Desktop
+import java.net.URI
+
+val httpClient: HttpClient = HttpClient(CIO) {
+    install(Logging) {
+        level = LogLevel.ALL
+    }
+    install(ContentNegotiation) {
+        json()
+    }
+}
 
 suspend fun main(): Unit = supervisorScope {
+    val mokt = MSAuth.OAuth(
+        clientId = "1e8287cd-e119-4829-877c-10a13996dedd",
+        httpClient = httpClient
+    )
 
+    val msTokenResponse = mokt.getMSToken(
+        openInBrowser = { Desktop.getDesktop().browse(URI(it)) }
+    )
+
+    val xBoxTokenResponse = mokt.getXboxToken(msTokenResponse.accessToken)
+    val xstsToken = mokt.getXstsToken(xBoxTokenResponse)
+    println(mokt.getMojangAuthToken(xstsToken))
 }
