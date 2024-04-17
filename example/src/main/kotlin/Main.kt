8dfa9722f9096c852334f75a9bbb48affa193c40
Redtronics <nils.jaekel@proton.me>
2024-04-17 21:34:51 +0200

    fix: compile error

--- example/src/main/kotlin/Main.kt
+++ example/src/main/kotlin/Main.kt
@@ -7,8 +7,6 @@ import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.coroutines.supervisorScope
-import java.awt.Desktop
-import java.net.URI
 
 val httpClient: HttpClient = HttpClient(CIO) {
     install(Logging) {
@@ -20,16 +18,8 @@ val httpClient: HttpClient = HttpClient(CIO) {
 }
 
 suspend fun main(): Unit = supervisorScope {
-    val mokt = MSAuth.OAuth(
+    val msAuth = MSAuth.OAuth(
         clientId = "1e8287cd-e119-4829-877c-10a13996dedd",
         httpClient = httpClient
     )
-
-    val msTokenResponse = mokt.getMSToken(
-        openInBrowser = { Desktop.getDesktop().browse(URI(it)) }
-    )
-
-    val xBoxTokenResponse = mokt.getXboxToken(msTokenResponse.accessToken)
-    val xstsToken = mokt.getXstsToken(xBoxTokenResponse)
-    println(mokt.getMojangAuthToken(xstsToken))
 }
