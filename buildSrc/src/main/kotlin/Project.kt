ceb56e6963e7675edea5b14c887f51d005eb608d
Nils Jäkel <nils.jaekel@proton.me>
2024-09-20 21:51:34 +0200

    feat: added some project settings

---
+++ buildSrc/src/main/kotlin/Project.kt
@@ -0,0 +1,18 @@
+/*
+ * MIT License
+ * Copyright 2024 Nils Jäkel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software.
+ */
+
+object Project {
+    const val NAME = "Mokt"
+    const val DESCRIPTION = "A Kotlin Multiplatform SDK for interaction with the Minecraft World."
+    const val GROUP = "dev.redtronics.mokt"
+    const val URL = "https://mokt.redtronics.dev"
+    const val GITLAB_URL = "https://code.redtronics.dev"
+}
