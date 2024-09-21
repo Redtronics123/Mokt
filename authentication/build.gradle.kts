74725458aa22ea922fcc85cc57b1b786717c36c3
Nils Jäkel <nils.jaekel@proton.me>
2024-09-21 15:53:10 +0200

    feat: added ktor server deps

--- authentication/build.gradle.kts
+++ authentication/build.gradle.kts
@@ -6,10 +6,17 @@ plugins {
 group = Project.GROUP
 
 kotlin {
+    linuxX64()
+
     sourceSets {
         commonMain {
             dependencies {
                 implementation(project(":common"))
+
+                implementation(libs.ktor.server.core)
+                implementation(libs.ktor.server.cio)
+                implementation(libs.ktor.server.content.negotiation)
+                implementation(libs.ktor.server.html)
             }
         }
 
