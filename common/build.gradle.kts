770ec2eed107df834b96a56f95bf604361143af5
Nils Jäkel <nils.jaekel@proton.me>
2024-09-21 13:53:02 +0200

    feat: added cinterop and multiplatform setup

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,3 +1,38 @@
+import kotlin.io.path.Path
+
 plugins {
     `mokt-publishing`
+    `mokt-multiplatform`
+    `mokt-cinterop-generation`
+}
+
+group = Project.GROUP
+
+kotlin {
+    js(IR) {
+        generateTypeScriptDefinitions()
+        nodejs()
+        useEsModules()
+        binaries.library()
+    }
+
+    linuxX64 {
+        applyCInteropGeneration(Path("../native-cinterop/aarch64-unknown-linux-gnu.def"))
+    }
+
+    mingwX64 {
+        applyCInteropGeneration(Path("../native-cinterop/x86_64-pc-windows-gnu.def"))
+    }
+
+    sourceSets {
+        all {
+            languageSettings {
+               optIn("kotlinx.cinterop.UnsafeNumber")
+               optIn("kotlinx.cinterop.ExperimentalForeignApi")
+               optIn("kotlin.experimental.ExperimentalNativeApi")
+               optIn("kotlin.native.runtime.NativeRuntimeApi")
+               optIn("kotlin.ExperimentalStdlibApi")
+            }
+        }
+    }
 }
