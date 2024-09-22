abb960724b644cfc3648069b0f30c41e6db5c8ce
Nils Jäkel <nils.jaekel@proton.me>
2024-09-22 20:24:02 +0200

    feat: added Microsoft provider

---
+++ authentication/src/commonMain/kotlin/dev/redtronics/mokt/provider/Microsoft.kt
@@ -0,0 +1,40 @@
+/*
+ * MIT License
+ * Copyright 2024 Nils Jäkel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software.
+ */
+
+package dev.redtronics.mokt.provider
+
+import dev.redtronics.mokt.Provider
+import dev.redtronics.mokt.provider.builder.MSDeviceAuthBuilder
+import dev.redtronics.mokt.provider.builder.MSOAuthBuilder
+
+public enum class MSAuthMethod(public val authMethodName: String) {
+    OAUTH2("oauth2"),
+    DEVICE_AUTH("device_auth");
+
+    public companion object {
+        public fun byName(name: String): MSAuthMethod = entries.first { it.authMethodName == name }
+    }
+}
+
+public class Microsoft : Provider {
+    override val name: String
+        get() = "Microsoft"
+
+    public var authMethod: MSAuthMethod = MSAuthMethod.OAUTH2
+
+    public suspend fun oauth2(builder: suspend MSOAuthBuilder.() -> Unit) {
+
+    }
+
+    public suspend fun device(builder: suspend MSDeviceAuthBuilder.() -> Unit) {
+
+    }
+}
