b09f3307f86e95877d367cff845a3c92583fd0fb
Nils Jäkel <nils.jaekel@proton.me>
2024-09-29 12:55:02 +0200

    feat: added available os types

--- common/src/commonMain/kotlin/dev/redtronics/mokt/System.kt
+++ common/src/commonMain/kotlin/dev/redtronics/mokt/System.kt
@@ -1,3 +1,16 @@
 package dev.redtronics.mokt
 
 public expect fun getEnv(key: String): String?
+
+public enum class OsType(public val value: String) {
+    WINDOWS("windows"),
+    LINUX("linux"),
+    ANDROID("android"),
+    UNKNOWN("unknown");
+
+    public companion object {
+        public fun byName(name: String): OsType = entries.firstOrNull { it.value == name } ?: UNKNOWN
+    }
+}
+
+public expect val os: OsType
