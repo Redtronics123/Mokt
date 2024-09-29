84fd201fde355393582c07bf9d0ded4f9fb72038
Nils Jäkel <nils.jaekel@proton.me>
2024-09-29 12:43:14 +0200

    feat: impl open url in browser for native os

---
+++ common/src/mingwMain/kotlin/dev/redtronics/mokt/Browser.mingw.kt
@@ -0,0 +1,22 @@
+/*
+ * MIT License
+ * Copyright 2024 Nils Jäkel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software.
+ */
+
+package dev.redtronics.mokt
+
+import dev.redtronics.mokt.cinterop.open_url
+import io.ktor.http.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.withContext
+
+public actual suspend fun openInBrowser(url: Url): Unit = withContext(Dispatchers.IO) {
+    open_url(url.toString())
+}
