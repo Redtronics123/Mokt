4aed3932e2e04f010a8372f2fcb8d3037097fc74
Nils Jäkel <nils.jaekel@proton.me>
2024-09-29 13:56:29 +0200

    feat: added build validation func

--- authentication/src/commonMain/kotlin/dev/redtronics/mokt/provider/builder/MSAuthBuilder.kt
+++ authentication/src/commonMain/kotlin/dev/redtronics/mokt/provider/builder/MSAuthBuilder.kt
@@ -12,22 +12,52 @@
 package dev.redtronics.mokt.provider.builder
 
 import dev.redtronics.mokt.getEnv
+import dev.redtronics.mokt.openInBrowser
 import io.ktor.http.*
 
 public sealed class MsAuth {
-
+    /**
+     * Builds the authentication configuration and validates it.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
+    internal abstract fun build()
 }
 
 public class MSDeviceAuthBuilder : MsAuth() {
+    override fun build() {
 
+    }
 }
 
 public class MSOAuthBuilder internal constructor() : MsAuth() {
+    /**
+     * The local redirect URL.
+     *
+     * On default, it will try to get the url from the environment variable `LOCAL_REDIRECT_URL`.
+     * Otherwise, the url `http://localhost:8080` will be used.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     public var localRedirectUrl: Url = Url(getEnv("LOCAL_REDIRECT_URL") ?: "http://localhost:8080")
 
+    /**
+     * The port of the local redirect URL.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     public val localRedirectPort: Int
         get() = localRedirectUrl.port
 
+    /**
+     * The given host of the local redirect URL.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     public val localRedirectHost: String
         get() = localRedirectUrl.host
 
@@ -40,7 +70,13 @@ public class MSOAuthBuilder internal constructor() : MsAuth() {
      * */
     public var requireHttpsByRedirect: Boolean = false
 
-    public suspend fun msAccessToken() {
+    public suspend fun accessToken(
+        browser: suspend (url: Url) -> Unit = {  url -> openInBrowser(url) }
+    ) {
 
+    }
+
+    override fun build() {
+        if (requireHttpsByRedirect && !localRedirectUrl.protocol.isSecure()) throw IllegalArgumentException("Local redirect URL is not using HTTPS")
     }
 }
