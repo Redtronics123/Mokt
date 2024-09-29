766a332259c1a91d36dee1b40bf3a81092938513
Nils Jäkel <nils.jaekel@proton.me>
2024-09-29 12:56:10 +0200

    feat: impl os type resolve for the jvm

--- common/src/jvmMain/kotlin/dev/redtronics/mokt/System.jvm.kt
+++ common/src/jvmMain/kotlin/dev/redtronics/mokt/System.jvm.kt
@@ -1,3 +1,12 @@
 package dev.redtronics.mokt
 
 public actual fun getEnv(key: String): String? = System.getenv(key)
+
+public actual val os: OsType by lazy {
+    val osName = System.getProperty("os.name").lowercase()
+    when {
+        osName.contains(other = "win") -> OsType.WINDOWS
+        osName.contains(other = "nix") || osName.contains(other = "nux") || osName.contains(other = "aix") -> OsType.LINUX
+        else -> { OsType.UNKNOWN }
+    }
+}
