87e570c7cf117bf1c6e1d64b06d7e9748353b358
Nils Jäkel <nils.jaekel@proton.me>
2024-09-29 12:42:54 +0200

    feat: added except func to open a browser

---
+++ common/src/commonMain/kotlin/dev/redtronics/mokt/Browser.kt
@@ -0,0 +1,16 @@
+/*
+ * MIT License
+ * Copyright 2024 Nils Jäkel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software.
+ */
+
+package dev.redtronics.mokt
+
+import io.ktor.http.*
+
+public expect suspend fun openInBrowser(url: Url)
