7032053da5d6b60c425d5a4d1260582add43ba43
Nils Jäkel <nils.jaekel@proton.me>
2024-09-29 12:04:47 +0200

    feat: added func to open url with native browser

---
+++ mokt-rust-bindings/src/browser.rs
@@ -0,0 +1,42 @@
+/*
+ * MIT License
+ * Copyright 2024 Nils Jäkel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software.
+ */
+
+use std::ffi::CStr;
+use std::process::Command;
+use libc::c_char;
+
+#[no_mangle]
+pub extern "C" fn open_url(url: *const c_char) {
+    let url = unsafe {
+        assert!(
+            !url.is_null(),
+            "url must not be null"
+        );
+        CStr::from_ptr(url)
+    }.to_str().unwrap();
+
+    if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .arg("/C")
+            .arg("start")
+            .arg(url)
+            .spawn()
+            .expect("failed to execute process");
+        return;
+    }
+
+    Command::new("sh")
+        .arg("-c")
+        .arg("xdg-open")
+        .arg(url)
+        .spawn()
+        .expect("failed to execute process");
+}
