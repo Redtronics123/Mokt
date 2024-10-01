801e718f7ccccfa85ba1331ce3d8b42329801ad1
Nils Jäkel <nils.jaekel@proton.me>
2024-10-01 10:00:20 +0200

    feat: added error code as unique response

--- authentication/src/commonMain/kotlin/dev/redtronics/mokt/microsoft/response/MSOAuthResponse.kt
+++ authentication/src/commonMain/kotlin/dev/redtronics/mokt/microsoft/response/MSOAuthResponse.kt
@@ -11,7 +11,6 @@
 
 package dev.redtronics.mokt.microsoft.response
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -19,27 +18,3 @@ public data class OAuthCode(
     public val code: String,
     public val state: Int
 )
-
-@Serializable
-public data class OAuthError(
-    public val error: OAuthErrorItem,
-    @SerialName("error_description")
-    public val errorDescription: String
-)
-
-@Serializable
-public enum class OAuthErrorItem(public val value: String) {
-    INVALID_REQUEST("invalid_request"),
-    UNAUTHORIZED_CLIENT("unauthorized_client"),
-    ACCESS_DENIED("access_denied"),
-    UNSUPPORTED_RESPONSE_TYPE("unsupported_response_type"),
-    SERVER_ERROR("server_error"),
-    TEMPORARY_UNAVAILABLE("temporarily_unavailable"),
-    INVALID_RESOURCE("invalid_resource"),
-    LOGIN_REQUIRED("login_required"),
-    INTERACTION_REQUIRED("interaction_required");
-
-    public companion object {
-        public fun byName(name: String): OAuthErrorItem = entries.first { it.value == name }
-    }
-}
