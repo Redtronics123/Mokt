aaeee87a204ff4965c21a5cf052e92daa8b3edbb
Nils Jäkel <nils.jaekel@proton.me>
2024-10-05 18:13:28 +0200

    refactor: moved to other package

--- authentication/src/commonMain/kotlin/dev/redtronics/mokt/provider/response/AccessResponse.kt
+++ authentication/src/commonMain/kotlin/dev/redtronics/mokt/provider/response/AccessResponse.kt
@@ -1,8 +1,9 @@
 /*
  * MIT License
  * Copyright 2024 Nils Jäkel
+ *
  * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the "Software”),
+ * a copy of this software and associated documentation files (the “Software”),
  * to deal in the Software without restriction, including without limitation
  * the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software.
@@ -14,15 +15,16 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-public data class DeviceCodeResponse(
-    @SerialName("device_code")
-    public val deviceCode: String,
-    @SerialName("user_code")
-    public val userCode: String,
-    @SerialName("verification_uri")
-    public val verificationUri: String,
+public data class AccessResponse(
+    @SerialName("token_type")
+    public val tokenType: String,
+    public val scope: String,
     @SerialName("expires_in")
     public val expiresIn: Int,
-    public val interval: Int,
-    public val message: String
+    @SerialName("ext_expires_in")
+    public val extExpiresIn: Int,
+    @SerialName("access_token")
+    public val accessToken: String,
+    @SerialName("refresh_token")
+    public val refreshToken: String
 )
