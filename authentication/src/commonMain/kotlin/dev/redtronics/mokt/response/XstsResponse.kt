5b579f4ab13f4c1e66019939eb7c32a3c5b50166
Nils Jäkel <nils.jaekel@proton.me>
2024-10-06 20:31:25 +0200

    feat: added xsts response

---
+++ authentication/src/commonMain/kotlin/dev/redtronics/mokt/response/XstsResponse.kt
@@ -0,0 +1,26 @@
+/*
+ * MIT License
+ * Copyright 2024 Nils Jäkel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software.
+ */
+
+package dev.redtronics.mokt.response
+
+import kotlinx.datetime.Instant
+import kotlinx.serialization.SerialName
+
+public data class XstsResponse(
+    @SerialName("IssueInstant")
+    public val issueInstant: Instant,
+    @SerialName("NotAfter")
+    public val notAfter: Instant,
+    @SerialName("Token")
+    public val token: String,
+    @SerialName("DisplayClaims")
+    public val displayClaims: DisplayClaims
+)
