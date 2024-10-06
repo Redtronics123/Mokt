48aa6725150e691d06a2e549700a85cbd70867ae
Nils Jäkel <nils.jaekel@proton.me>
2024-10-06 20:30:08 +0200

    feat: added display claims

---
+++ authentication/src/commonMain/kotlin/dev/redtronics/mokt/response/DisplayClaims.kt
@@ -0,0 +1,24 @@
+/*
+ * MIT License
+ * Copyright 2024 Nils Jäkel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software.
+ */
+
+package dev.redtronics.mokt.response
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+public data class DisplayClaims(
+    public val xui: List<Uhs>
+)
+
+@Serializable
+public data class Uhs(
+    public val uhs: String
+)
