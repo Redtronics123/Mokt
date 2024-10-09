532ea6925142ea8712ec9c33d77dd35272261800
Nils Jäkel <nils.jaekel@proton.me>
2024-10-09 10:12:30 +0200

    feat: added logic to get the xsts token response

--- authentication/src/commonMain/kotlin/dev/redtronics/mokt/builder/XstsBuilder.kt
+++ authentication/src/commonMain/kotlin/dev/redtronics/mokt/builder/XstsBuilder.kt
@@ -9,19 +9,53 @@
  * and/or sell copies of the Software.
  */
 
+@file:Suppress("MemberVisibilityCanBePrivate")
+
 package dev.redtronics.mokt.builder
 
+import dev.redtronics.mokt.payload.XstsPayload
+import dev.redtronics.mokt.payload.XstsProperties
 import dev.redtronics.mokt.response.XBoxResponse
 import dev.redtronics.mokt.response.XstsResponse
 import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
 import kotlinx.serialization.json.Json
 
 public class XstsBuilder internal constructor(
     override val httpClient: HttpClient,
     override val json: Json,
-    xBoxResponse: XBoxResponse
+    private val xBoxResponse: XBoxResponse
 ) : BaseBuilder() {
+    public var sandboxId: String = "RETAIL"
+
+    public var relyingParty: String = "rp://api.minecraftservices.com"
+
+    public val authUrl: Url
+        get() = Url("https://xsts.auth.xboxlive.com/xsts/authorize")
+
     internal suspend fun build(onRequestError: suspend () -> Unit): XstsResponse?  {
-        return null
+        val xstsPayload = XstsPayload(
+            properties = XstsProperties(
+                sandboxId = sandboxId,
+                userTokens = listOf(xBoxResponse.token)
+            ),
+            relyingParty = relyingParty,
+            tokenType = TokenType.JWT
+        )
+
+        val response = httpClient.post {
+            url(authUrl)
+            contentType(ContentType.Application.Json)
+            setBody(json.encodeToString(XstsPayload.serializer(), xstsPayload))
+        }
+
+        if (!response.status.isSuccess()) {
+            onRequestError()
+            return null
+        }
+
+        return json.decodeFromString(XstsResponse.serializer(), response.bodyAsText())
     }
 }
